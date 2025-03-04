ff222b5488bd3715f92ba2867178032a37013828
Nubunduram <ceantoine@hotmail.fr>
2025-03-04 12:07:02 +0100

    feat: Dish isNew ? (activite 9)

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,13 @@ function App() {
       <Container>
         <Row>
           <Col md={4}>
-            <Dish title={"Tacos à l’unité"} price={3} img={"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"} />
+            <Dish title={"Tacos à l’unité"} price={3} img={"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"} isNew={true}/>
           </Col>
           <Col md={4}>
-            <Dish title={"Enchiladas"} price={12} img={"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"} />
+            <Dish title={"Enchiladas"} price={12} img={"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"} isNew={false} />
           </Col>
           <Col md={4}>
-            <Dish title={"Mole poblano"} price={15} img={"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"} />
+            <Dish title={"Mole poblano"} price={15} img={"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"} isNew={false} />
           </Col>
         </Row>
       </Container>
--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Card } from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
 import "./Dish.scss";
 
-const Dish = ({ title, price, img }) => {
+const Dish = ({ title, price, img, isNew }) => {
     return (
         <Card className='Dish'>
+            {isNew && <Badge className='badge' bg="primary">Nouveau</Badge>}
             <Card.Img src={img} alt={title} />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>

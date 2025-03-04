954d957619297c3dbc271e9b20bb5d5a728cb55a
Nubunduram <ceantoine@hotmail.fr>
2025-03-04 14:23:50 +0100

    feat: Add to basket button (Activite 12)

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Card, Badge } from 'react-bootstrap';
+import { Card, Badge, Button } from 'react-bootstrap';
 import "./Dish.scss";
 
 const Dish = ({ title, price, img, isNew }) => {
     return (
-        <Card className='Dish'>
+        <Card>
             {isNew && <Badge bg="primary">Nouveau</Badge>}
+            
             <Card.Img src={img} alt={title} />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>{price}€</Card.Text>
+                <Button onClick={()=>alert("Le plat Nom du plat est maintenant dans votre panier.")} variant="primary">Primary</Button>
             </Card.Body>
+
         </Card>
     );
 };

e48471002ccc971e5e099c7d579eac64984d5933
Nubunduram <ceantoine@hotmail.fr>
2025-03-04 11:44:22 +0100

    fix: Bootstrap & scss (Activite 7)

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ function App() {
       <Header />
       <Container>
         <Row>
-          <Col>
+          <Col md={4}>
             <Dish title={"Tacos à l’unité"} price={3} img={"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"} />
           </Col>
-          <Col>
+          <Col md={4}>
             <Dish title={"Enchiladas"} price={12} img={"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"} />
           </Col>
-          <Col>
+          <Col md={4}>
             <Dish title={"Mole poblano"} price={15} img={"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"} />
           </Col>
         </Row>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import logo from '../../assets/man.webp'
+import React from 'react';
+import logo from '../../assets/man.webp';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import './Header.scss';
 
 const Header = () => {
     return (
         <header>
-            <Navbar>
+            <Navbar className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand href="#home">
-                        <img className='logo' src={logo}></img>
+                    <Navbar.Brand href="/">
+                        <img className="logo" src={logo} alt="Logo" />
                     </Navbar.Brand>
-                    <Nav>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
